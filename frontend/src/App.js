ea8a85d2ee8aa24af6942c46e2de28de444f4ecb
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-25 10:36:05 +0530

    route handler updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Nav from './components/nav/Nav';
 import SignUp from './components/signup/Signup';
+import { RouteHandler } from './components/router/RouteHandler';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Nav/>
-     
+     <RouteHandler/>
     </div>
   );
 }
