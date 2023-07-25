737d1deadd2cb39001cb30082727ae2fea874511
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-25 10:52:52 +0530

    Added Home page

--- frontend/src/components/router/RouteHandler.jsx
+++ frontend/src/components/router/RouteHandler.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import LogIn from "../login/Login";
 import SignUp from "../signup/Signup";
+import Home from "../home/Home";
 export const RouteHandler = () =>{
     return(
         <>   
             <Routes>
-              
-              <Route path="/" element={<LogIn/>}/>
+              <Route path="/Home" element={<Home/>}/>
+              <Route path="/Login" element={<LogIn/>}/>
               <Route path="/SignUp" element={<SignUp/>}/>
-              
             </Routes>
         </>
     );
