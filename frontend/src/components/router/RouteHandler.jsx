c5da5d46c50eb4defc2e7ada6b08eb7ecb0d3ee5
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-25 11:37:46 +0530

    Added Admin Login Page

--- frontend/src/components/router/RouteHandler.jsx
+++ frontend/src/components/router/RouteHandler.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import LogIn from "../login/Login";
 import SignUp from "../signup/Signup";
 import Home from "../home/Home";
+import AdminLogIn from "../adminLogin/AdminLogin";
 export const RouteHandler = () =>{
     return(
         <>   
@@ -9,6 +10,7 @@ export const RouteHandler = () =>{
               <Route path="/Home" element={<Home/>}/>
               <Route path="/Login" element={<LogIn/>}/>
               <Route path="/SignUp" element={<SignUp/>}/>
+              <Route path='/admin' element={<AdminLogIn/>}/>
             </Routes>
         </>
     );
